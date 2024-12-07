126149009d59e12b5c4ee38f73dec40619450d64
Jaya Raj <66004722+JayaRaj0908@users.noreply.github.com>
2024-12-07 11:50:54 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -3,3 +3,10 @@ factorial = 1
 for i in range(1, num+1):
     factorial *= i
 print(f"Factorial of {num} is {factorial}")
+
+
+n = int(input("Enter a number = "))
+def factorial(n):
+    return 1 if(n==0 or n==1) else n*factorial(n-1)
+
+print(f"Factorial of {n} is {factorial(n)}")
